6c6eaf8a2c2ee728bb5330c7a73212eef19555f0
tarunsai <67566514+TARUNSAI77022@users.noreply.github.com>
2026-02-24 16:02:21 +0530

    Replace buggy az login script with robust Terraform Bootstrap method

--- environments/bootstrap/main.tf
+++ environments/bootstrap/main.tf
@@ -11,13 +11,19 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
 resource "azurerm_resource_group" "tfstate" {
   name     = "rg-terraform-state"
   location = "South India"
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstatergtestdev123"
+  name                     = "tfstatetestdev${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.tfstate.name
   location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
